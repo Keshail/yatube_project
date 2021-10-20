d344c71fd3abab820b07c8426318c5b1297b89d9
Nikita <riki.kizh@yandex.ru>
2021-10-20 22:31:52 +0300

    Добавлена папка static и внесена пара вправок

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from . import views
+
+app_name = 'posts'
 
 urlpatterns = [
     path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('posts/group_list.html', views.group_posts, name='group_list'),
+    path('index.html', views.index, name='index'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def group_posts(request):
+    template = "posts/group_list.html"
+    title = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
 
 def index(request):
-    template = 'posts/index.html'
-    return render(request, template) 
-
-
-# Страница со списком мороженого
-def group_posts(request):
-    template = 'posts/group_list.html'
-    return render(request, template)
+    template = "posts/index.html"
+    title = "Это главная страница проекта Yatube"
+    context = {
+        'title': title,
+    }
+    return render(request, template, context) 

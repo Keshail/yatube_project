b6e85ac68ea74863505f22a6eb733a44b8d6fccd
Nikita <riki.kizh@yandex.ru>
2021-10-28 00:48:20 +0300

    Переделанно все и добавлены модели

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index),
-    path('posts/group_list.html', views.group_posts, name='group_list'),
-    path('index.html', views.index, name='index'),
+    path('group/<slug>/', views.group_posts, name='group_list'),
+    path('', views.index, name='index'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
-def group_posts(request):
-    template = "posts/group_list.html"
-    title = "Здесь будет информация о группах проекта Yatube"
-    context = {
-        'title': title,
-    }
-    return render(request, template, context)
+from .models import Post, Group
 
 def index(request):
-    template = "posts/index.html"
-    title = "Это главная страница проекта Yatube"
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
     context = {
+        'posts': posts,
         'title': title,
     }
-    return render(request, template, context) 
+    return render(request, 'posts/index.html', context)
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group}'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
